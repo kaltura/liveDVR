74f076045c3fe367829e98ff33851357ff09757c
elad.benedict <elad.benedict@kaltura.com>
2015-08-19 12:39:58 +0300

    Adding basic CI tasks

---
+++ tests/M3U8GeneratorTests.js
@@ -0,0 +1,24 @@
+/**
+ * Created by elad.benedict on 8/12/2015.
+ */
+
+var m3u8 = require('m3u8');
+PlaylistItem = require('m3u8/m3u/PlaylistItem');
+
+describe('M3U8 Generator tests', function() {
+    it('should create a new M3U8 generator', function (done) {
+        var m3u8Generator = require('../lib/M3U8Generator')("c:\\debug\\", "a.m3u8");
+        var promise = m3u8Generator.init();
+        promise.then(function(currentManifest)
+        {
+            var item = new PlaylistItem();
+            item.set('duration', 30.0);
+            item.set('uri', "uriName");
+            return m3u8Generator.update([item]);
+        })
+        promise.done(function()
+        {
+            done();
+        });
+    });
+});
