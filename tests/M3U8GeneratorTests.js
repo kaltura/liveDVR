6195e0879558ec3810d4dd0d1daf037efef8a5e0
elad.benedict <elad.benedict@kaltura.com>
2015-08-19 13:21:01 +0300

    Comment out half-baked test

--- tests/M3U8GeneratorTests.js
+++ tests/M3U8GeneratorTests.js
@@ -6,7 +6,7 @@ var m3u8 = require('m3u8');
 PlaylistItem = require('m3u8/m3u/PlaylistItem');
 
 describe('M3U8 Generator tests', function() {
-    it('should create a new M3U8 generator', function (done) {
+    /* it('should create a new M3U8 generator', function (done) {
         var m3u8Generator = require('../lib/M3U8Generator')("c:\\debug\\", "a.m3u8");
         var promise = m3u8Generator.init();
         promise.then(function(currentManifest)
@@ -20,5 +20,5 @@ describe('M3U8 Generator tests', function() {
         {
             done();
         });
-    });
+    }); */
 });
