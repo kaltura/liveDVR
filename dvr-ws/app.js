c4690496c2a6483ae6c2b32fa9bb47ecd5e344cf
sabanas <ace.zoo.run-080>
2015-09-09 11:22:56 +0300

    ws - replace morgan logger with winston logger

--- dvr-ws/app.js
+++ dvr-ws/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger = require('./logger');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('../lib/Configuration');
@@ -14,17 +13,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(config.get('rootFolderPath')));
 
+// express-winston logger makes sense BEFORE the router.
+app.use(logger.consoleLogger);
 
 app.use('/', routes);
+
+app.use(logger.errorLogger);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
