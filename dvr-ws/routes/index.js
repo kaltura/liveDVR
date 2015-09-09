e132ee70a54a83cde0d0c9321179cf9724ec4bbb
sabanas <ace.zoo.run-080>
2015-09-09 15:39:49 +0300

    ws changes

--- dvr-ws/routes/index.js
+++ dvr-ws/routes/index.js
@@ -1,32 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var MasterManifestGenerator = require('../../lib/MasterManifestGenerator');
+var MasterManifestCreator = require('../../lib/MasterManifestGenerator');
 var config = require('../../lib/Configuration');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+
+router.get('/error', function(req, res, next) {
+    // here we cause an error in the pipeline so we see express-winston in action.
+    return next(new Error("This is an error and it should be logged to the console"));
 });
 
-router.get('/application/:app_name/entry/:entry_id/tag/:tag/playlist.m3u8', function(req, res) {
+router.get('/:app_name/smil::entrySmil.smil/playlist.m3u8', function(req, res) {
 
-  console.log('application' + req.params['app_name'] + " , entryid: " + req.params['entry_id'] + " , tag: " + req.params['tag']);
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  console.log(fullUrl);
+    var entrySmil = req.params['entrySmil'];
+    var tagIndex = entrySmil.lastIndexOf("_");
+    var entryId = entrySmil.substring(0, tagIndex);
+    var tag = entrySmil.substring(tagIndex + 1);
 
-  var masterManifestGenerator = MasterManifestGenerator(req.params['entry_id'], config.get("mediaServer").hostname, config.get("mediaServer").port, req.params['app_name']);
+    var masterManifestCreator = MasterManifestCreator(entryId, config.get("mediaServer").hostname, config.get("mediaServer").port, req.params['app_name']);
 
-  //get master manifest
-  masterManifestGenerator.getManifest(fullUrl,req.params['tag']).then(
-      function(m3u8) {
-        res.send(m3u8.toString());
-      },
-      function(err) {
-          res.status(500).send({ error: err }); //TODO better error
-      }
-  );
+    //get master manifest
+    masterManifestCreator.getManifest(fullUrl,tag).then(
+        function(m3u8) {
+            res.send(m3u8.toString());
+        },
+        function(err) {
+            return next(new Error("Failed to get m3u8 manifest " + err + err.stack));
+        }
+    );
 });
-
 
 module.exports = router;
