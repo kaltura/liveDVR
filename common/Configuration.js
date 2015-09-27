a8fa9102c414919fb29ba6c9964d5d3f3ea7a8f9
elad.benedict <elad.benedict@kaltura.com>
2015-09-27 11:25:28 +0300

    * Created configuration template from which a config file with the current machine name is generated
    * Updated log paths to be machine local (these will be rotated and copied to a central location by Prod IT)
    * Add port logging for the web server

--- common/Configuration.js
+++ common/Configuration.js
@@ -3,7 +3,14 @@
  */
 
 var path = require('path');
+var fs = require('fs');
+
 module.exports = (function(){
+
+    var machineName = require('./utils/hostname');
+    var configTemplateContent = fs.readFileSync(path.join(__dirname, './config/config.json.template'), 'utf8');
+    var updatedConfigContent = configTemplateContent.replace('@HOSTNAME@', machineName);
+    fs.writeFileSync(path.join(__dirname, './config/config.json'), updatedConfigContent);
 
     var nconf = require('nconf');
 
