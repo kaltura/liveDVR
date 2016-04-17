3f87746fc11708daee07a304711eda1bb3407e3f
Ron Yadgar <ron.yadgar@Rons-MacBook-Pro.local>
2016-04-17 23:27:56 +0300

    Change logger so the module name is also  written

--- common/logger/logger.js
+++ common/logger/logger.js
@@ -17,7 +17,6 @@ var logger = function (file, level, logToConsole) {
     var log4js = require( "log4js" );
     var appenders = [
         {
-            "category": "newHLS",
             "type": "file",
             "filename": logFullPath,
             "timezoneOffset" : 300 // NYC timezone offset relative to UTC (5 * 60)
@@ -32,16 +31,12 @@ var logger = function (file, level, logToConsole) {
                 "type": "pattern",
                 pattern: "%d{ABSOLUTE} %[%-5p%] %c %m"
             },
-            "category": "newHLS"
         });
     }
 
     var log4jsConfiguration = {
         "appenders": appenders,
         "replaceConsole": false,
-        "levels" : {
-            "newHLS": level
-        }
     };
 
     log4js.configure(log4jsConfiguration);
@@ -52,7 +47,7 @@ var logger = function (file, level, logToConsole) {
         log4js.configure(log4jsConfiguration);
     });
 
-    var res = log4js.getLogger("newHLS");
+    var res = log4js.getLogger("[PID:"+process.pid+ "]");
     return res;
 };
 
