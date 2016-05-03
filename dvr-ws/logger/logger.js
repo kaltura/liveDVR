39000621a39be62a41c78e0e7cbb8413065d4c42
igorshevach <igor.shevach@kaltura.com>
2016-05-03 18:01:41 +0300

    - remove web server files

--- dvr-ws/logger/logger.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Elad Benedict on 8/24/15.
- */
-
-var commonLogger = require('../../common/logger/logger');
-var config = require('../../common/Configuration');
-var logger = commonLogger(config.get('webServerParams:logFileName'), config.get('webServerParams:logLevel'), config.get('webServerParams:logToConsole'));
-
-module.exports = logger;
--- packager/bin/run_nginx.sh
+++ packager/bin/run_nginx.sh
@@ -2,18 +2,46 @@
 
 dirname=`dirname $0`
 
+scriptName=`basename $0`
+
 os_name=`uname`
 
-confDir=`readlink -f $dirname/../config/`
+confDir=$dirname/../config/
 
-case  "$os_name" in
-case "Darwin")
-    binDir=darwin;;
-case "Linux")
-    binDir=linux/;;
+echo "confDir=$confDir"
+
+case  $os_name in
+"Darwin")
+    binDir=darwin
+    ;;
+"Linux")
+    binDir=linux/
+    ;;
+esac
 
 nginxPath=$dirname/$binDir/nginx
 
 sed "s#@CONTENT_DIR@#~/dvr/dvr/dvrContentRootPath/#" $confDir/nginx.conf.template > /var/tmp/nginx.conf
 
+function getNginxPids(){
+    ps -fA | grep nginx | grep -vE "grep|$scriptName" | awk '{print $2}'
+}
+
+getNginxPids
+
+processes=(`getNginxPids`)
+
+echo "processes=$processes"
+
+for p in ${processes[@]}
+do
+    echo "killing $p"
+    kill -9 $p
+done
+
+echo "running $nginxPath -c /var/tmp/nginx.conf"
+
 $nginxPath -c /var/tmp/nginx.conf &> /dev/null &
+
+echo "nginx pid(s) is:"
+getNginxPids
