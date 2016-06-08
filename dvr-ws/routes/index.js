ce4b986193f1aa7abd27df7219f30b63b8941b8b
lilachmaliniak <lilach.maliniak@IL-LILACHM-BE-1523.local>
2016-06-08 15:41:54 +0300

    change #1 of 3
    partial changes to support live stream failover, based on HLS RFC
    (draft-pantos-http-live-streaming-19), since October 6, 2016.
    Implementation incude 3 main changes:
    1. backdate of chunkiest file on stream state machine enter stopped
    state.
    2. return 404 to player, in case chunkiest age is too old.
    3. Add ENDLIST tag when server nodes (DC) serving the entry ID count,
    equals to zero.

--- dvr-ws/routes/index.js
+++ dvr-ws/routes/index.js
@@ -2,18 +2,26 @@ var express = require('express');
 var router = express.Router();
 var config = require('../../common/Configuration');
 var logger = require('../logger/logger');
-var qio = require('q-io/fs');
 var path = require('path')
 var persistenceFormat = require('../../common/PersistenceFormat');
 
 router.get(/\/smil\:([^\\/]*)_all\.smil\/([^\?]*)/i, function(req, res) {
-
+    var that = this;
     var entryId = req.params[0];
     var fileName = req.params[1];
 
-    var disk = path.join(persistenceFormat.getEntryFullPath(entryId),fileName);
-    res.sendFile(disk);
-
+    var fullpath = path.join(persistenceFormat.getEntryFullPath(entryId),fileName);
+    res.sendFile(fullpath);
+/*
+    checkFileModifiedTime(fullpath)
+        .then( function() {
+            res.sendFile(fullpath);
+        })
+        .catch( function() {
+            res.status(404).send('%s not found', that.filename);
+        });
+*/
 });
+
 
 module.exports = router;
