4df43fdc6be4aaa5ba80950922b4a02e7bfb0c25
Ron Yadgar <ron.yadgar@MacBook-Pro-9.local>
2016-07-31 10:52:27 +0300

    update unit test

--- tests/unit/RecordingManager-spec.js
+++ tests/unit/RecordingManager-spec.js
@@ -9,11 +9,58 @@ var Q = require('q');
 var config = require('../../common/Configuration');
 var path = require('path');
 
-describe('Recording manager spec', function() {
+    describe('Recording manager spec', function() {
 
-    var RecordingManager = proxyquire('./../lib/RecordingManager', mocks);
-    it('should return true when there is no folder of the entry', function(done)
+
+        var clock;
+
+        beforeEach(function () {
+            clock = sinon.useFakeTimers();
+        });
+
+        afterEach(function () {
+            clock.restore();
+        });
+
+
+    it('should return error promise, if makeDirectory is failed', function(done)
     {
-
+        var message = "makeDirectory is failed"
+        var mocks = {
+            'q-io/fs': {makeDirectory: sinon.stub().returns(Q.reject(message))}
+        };
+        var RecordingManager = proxyquire('../../lib/RecordingManager', mocks);
+        RecordingManager.newChunks("Mp4Files", "entryId", "liveSessionExpirationDuration").then(null,function (err) { console.log("makeDirectory is called")
+            done();
+        })
     })
+    it('should return error promise, if all file failed to link', function(done)
+    {
+        var message = "makeDirectory is failed"
+        var mocks = {
+            'q-io/fs': {makeDirectory: sinon.stub().returns(Q.reject(message))}
+        };
+        var RecordingManager = proxyquire('../../lib/RecordingManager', mocks);
+        RecordingManager.newChunks("Mp4Files", "entryId", "liveSessionExpirationDuration").then(null,function (err) {
+            expect(err).to.equal(message);
+            done();
+        })
+    })
+
+    it.only(' new chunk', function(done){
+
+        var mocks = {
+            'q-io/fs': {
+                makeDirectory: sinon.stub().returns(Q.resolve()),
+                list: sinon.stub().returns(Q.resolve({}))
+            }
+        }
+       // var RecordingManager = proxyquire('../../lib/RecordingManager', mocks);
+        console.log(new Date().getTime())
+        clock.tick(1);
+        console.log(new Date().getTime())
+       // RecordingManager.newChunks("Mp4Files", "entryId", "liveSessionExpirationDuration").then(null,function () {
+            console.log(onStartUp.callCount.to.equal(1));
+            done();
+        })
     })
