f20a4204e6190933e885aaf8ada42be60dd02007
lilach.maliniak <lilach.maliniak@kaltura.com>
2016-08-04 10:31:07 +0300

    added support in running regression tests using grunt task
    Added grunt-execute plugin to support regression tests as execute task.
    Work still in progress.
    Known issues:
    1) when polling interval is configured to 1millisecond regression
    sometimes fail on last chunklist checksum.
    2) found incorrect promises impl in
    HlsStreamRegressionValidator.prototype.addChunklist need re-write.
    
    Still missing:
    1) task for recording.
    2) writing all errors during validation aggregation to test results
    Json.
    3) integration with Jenkins.

--- gruntfile.js
+++ gruntfile.js
@@ -11,9 +11,12 @@ module.exports = function (grunt) {
     grunt.file.expand('./node_modules/grunt-*/tasks').forEach(grunt.loadTasks);
     grunt.initConfig(userConfig);
 
+    grunt.loadNpmTasks('grunt-execute');
+
     grunt.loadTasks('./lib/grunt/tasks');
     grunt.loadTasks('./lib/grunt/config');
 
+
     // Default task(s).
-    grunt.registerTask('default', ['jshint:dev','unit-test:dev', 'component-test:dev', 'mocha_istanbul:dev']);
+    grunt.registerTask('default', ['jshint:dev', 'execute:regression_test', 'unit-test:dev', 'component-test:dev', 'mocha_istanbul:dev']);
 };
