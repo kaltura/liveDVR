7a0957de4e9387ddcc0fd08dd3e2870499c8c265
lilach.maliniak <lilach.maliniak@kaltura.com>
2016-08-18 07:33:07 +0300

    regression tests is now working as mocha unit test
    Added events support to Controller.

--- tests/regression/regression-spec.js
+++ tests/regression/regression-spec.js
@@ -9,94 +9,11 @@ var sinon = require('sinon');
 var Q = require('Q');
 var should = chai.should();
 var ControllerCtor = require('./../../lib/Controller');
-var childProcess = require('child_process');
-
-// todo: check if this is required?????
-/*
-var loggerMock = {
-    info : sinon.stub(),
-    error : sinon.stub(),
-    debug : sinon.stub(),
-    warn : sinon.stub(),
-};
-
-// todo: check if this is required?????
-var mocks = {
-    './logger/logger' : sinon.stub().returns(loggerMock)
-};*/
+var util=require('util');
 
 
-describe('Regression test1', function() {
-
-    // Set an appropriate test timeout here to 12 hours: 12 * 3600 * 1000 = 43200000
-    this.timeout(43200000);
-
-    it('should run regression and receive same result as ground truth', function(done) {
-        var deferred = Q.defer();
-
-        const spawn = require('child_process').spawn;
-
-        const child = spawn(process.argv[0], ['lib/App.js'], {
-            stdio: ['inherit', 'inherit', 'inherit']
-        });
-
-        child.on('error', (err) => {
-            console.log('Failed to start child process.');
-            if (err.code != 0) {
-                let err_msg = util.format('failed to start liveController, error %s', err);
-                deferred.resolve()
-            }
-        });
-
-        child.on('close', (err) => {
-            if (err.code === 0) {
-                console.log('test finished successfully.');
-                deferred.resolve();
-            } else {
-                util.format('test failed with exit code '  + err);
-                console.log(err_msg);
-                deferred.resolve(err_msg);
-            }
-        });
-
-        //child.unref();
-
-        // wrap in promise
-        deferred.promise.then(function(msg) {
-            expect(err).to.equal(0);
-            done();
-        });
-    });
-
-});
-
-/*
-describe('Regression test2', function() {
-
-    // Set an appropriate test timeout here to 12 hours: 12 * 3600 * 1000 = 43200000
-    this.timeout(43200000);
-
-    it('should run regression and receive same result as ground truth', function(done) {
-        var controller = childProcess.exec('node lib/app.js', function (error, stdout, stderr) {
-        if (error) {
-            console.log(error.stack);
-            console.log('Error code: '+error.code);
-            console.log('Signal received: '+error.signal);
-        }
-        console.log('Child Process STDOUT: '+stdout);
-        console.log('Child Process STDERR: '+stderr);
-        });
-        controller.on('exit', function (code) {
-            console.log('Child process exited with exit code '+code);
-        });
-    });
-});
-*/
-
-
-/*
 //todo: in order for this to work need add to Controller, support in events.
-describe('Regression test3', function() {
+describe('Regression test4', function() {
 
     // Set an appropriate test timeout here to 12 hours: 12 * 3600 * 1000 = 43200000
     this.timeout(43200000);
@@ -104,16 +21,22 @@ describe('Regression test3', function() {
     it('should run regression and receive same result as ground truth', function(done) {
         var prefix = "";
         var controller = new ControllerCtor(prefix);
-        controller.start();
-        controller.on('event', function(code) {
+        controller.on('exit', function(code) {
             if (code === 0) {
                 // regression passed successfully
-                done();
+                console.log('*************************************************');
+                console.log('*     regression test finished successfully     *');
+                console.log('*************************************************');
             } else {
                 // regression failed!
-                done(err);
+                console.log('*************************************************');
+                console.log(util.format('@@@ regression test failed with error %s!!! @@@', code));
+                console.log('*************************************************');
             }
+            expect(code).to.equal(0);
+            done();
         });
+        controller.start();       
     });
 });
-*/
+
