fbb21b1b809bda4557717efa01487c1cb3d3f474
Ron Yadgar <ron5569@gmail.com>
2016-11-02 18:12:22 +0200

    update due to pull requst

--- recordingUploader/Tasks/UploadTask.py
+++ recordingUploader/Tasks/UploadTask.py
@@ -35,43 +35,44 @@ class UploadTask(TaskBase):
 
     def upload_file(self, file_name):
 
-        self.check_stamp()
+
         threadWorkers = ThreadWorkers()
         file_size = os.path.getsize(file_name)
         chunks_to_upload = self.get_chunks_to_upload(file_size)
-        infile = io.open(file_name, 'rb')
+        with io.open(file_name, 'rb') as infile:
 
-        upload_session = KalturaUploadSession(self.output_filename, file_size, chunks_to_upload,
-                                                self.entry_id, self.recorded_id, self.backend_client, self.logger, infile)
+            upload_session = KalturaUploadSession(self.output_filename, file_size, chunks_to_upload,
+                                                    self.entry_id, self.recorded_id, self.backend_client, self.logger, infile)
 
-        while upload_session.chunk_index < chunks_to_upload-1:
+            while upload_session.chunk_index < chunks_to_upload-1:
+                chunk = upload_session.get_next_chunk()
+                threadWorkers.add_job(chunk)
+
+            failed_jobs = threadWorkers.wait_jobs_done()
+            self.logger.info('Finish to upload [%s chunks], about to upload last chunk', chunks_to_upload-1)
+
+            # last chunk
             chunk = upload_session.get_next_chunk()
             threadWorkers.add_job(chunk)
+            job_result = threadWorkers.wait_jobs_done()
+            failed_jobs.extend(job_result)
+            self.check_stamp()
+            upload_session_json = str(vars(upload_session))
+            
+            if len(failed_jobs) == 0:
+                self.logger.info("successfully upload all chunks, call append recording")
 
-        failed_jobs = threadWorkers.wait_jobs_done()
-        self.logger.info('Finish to upload [%s chunks], about to upload last chunk', chunks_to_upload-1)
-
-        # last chunk
-        chunk = upload_session.get_next_chunk()
-        threadWorkers.add_job(chunk)
-        Job_result = threadWorkers.wait_jobs_done()
-        failed_jobs.extend(Job_result)
-        self.check_stamp()
-        upload_session_json = str(vars(upload_session))
-        if len(failed_jobs) == 0:
-            self.logger.info("successfully upload all chunks, call append recording")
-
-            #Check if need to call cancel_replace
-            recorded_obj = self.backend_client.get_recorded_entry(upload_session.partner_id, self.recorded_id)
-            if recorded_obj.replacementStatus.value != KalturaEntryReplacementStatus.NONE:
-                self.logger.info("entry %s has replacementStatus %s, calling cancel_replace", self.recorded_id,
-                                recorded_obj.replacementStatus)
-                self.backend_client.cancel_replace(upload_session.partner_id, self.recorded_id)
-            self.backend_client.set_recorded_content_remote(upload_session, str(float(self.duration)/1000))
-            os.rename(self.output_file_path, self.output_file_path + '.done')
-        else:
-            raise Exception("Failed to upload file, "+str(len(failed_jobs))+" chunks from "+str(chunks_to_upload)+ " where failed:"
-                            + upload_session_json)
+                #Check if need to call cancel_replace
+                recorded_obj = self.backend_client.get_recorded_entry(upload_session.partner_id, self.recorded_id)
+                if recorded_obj.replacementStatus.value != KalturaEntryReplacementStatus.NONE:
+                    self.logger.info("entry %s has replacementStatus %s, calling cancel_replace", self.recorded_id,
+                                    recorded_obj.replacementStatus)
+                    self.backend_client.cancel_replace(upload_session.partner_id, self.recorded_id)
+                self.backend_client.set_recorded_content_remote(upload_session, str(float(self.duration)/1000))
+                os.rename(self.output_file_path, self.output_file_path + '.done')
+            else:
+                raise Exception("Failed to upload file, "+str(len(failed_jobs))+" chunks from "+str(chunks_to_upload)+ " where failed:"
+                                + upload_session_json)
 
     def append_recording_handler(self):
         partner_id = self.backend_client.get_live_entry(self.entry_id).partnerId
@@ -79,6 +80,7 @@ class UploadTask(TaskBase):
                                                        str(float(self.duration)/1000))  # todo check it full path
 
     def run(self):
+        self.check_stamp()
         if get_config('mode') == 'remote':
             self.upload_file(self.output_file_path)
         else:
