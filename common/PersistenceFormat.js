7f0097d66d07ea17171d37a0ccad4a153cbefcb5
igorshevach <igor.shevach@kaltura.com>
2016-11-10 21:39:22 +0200

    further refactor of PersistenceFormat to differentiate b/w different modes

--- common/PersistenceFormat.js
+++ common/PersistenceFormat.js
@@ -11,64 +11,89 @@ var Q = require('q');
 const tsChunktMatch =  new RegExp(/media-([^_]+).*?([\d]+)\.ts.*/);
 var preserveOriginalHLS = config.get('preserveOriginalHLS').enable;
 
-module.exports = persistenceFormat = {
-
-    getEntryBasePath: function (entryId) {
+class PersistenceFormatBase {
+    getEntryBasePath (entryId) {
         return path.join(config.get('rootFolderPath'), entryId);
-    },
-
-    getBasePathFromFull: function (directory) {
-        return path.dirname(path.dirname(directory));
-    },
-
-    getRelativePathFromFull: function (fullPath) {
-        return fullPath.substr(persistenceFormat.getBasePathFromFull(fullPath).length);
-    },
-
-    getFlavorFullPath: function (entryId, flavorName) {
-        return path.join(config.get('rootFolderPath'), entryId, flavorName.toString());
-    },
-
-    getMasterManifestName: function () {
-        return 'playlist.json';
-    },
-    
-    getMP4FileNamefromInfo: function(chunkPath){
-         return chunkPath.replace('.ts','.mp4');
-    },
-
-
-    getTSChunknameFromMP4FileName: function(mp4FileName){
-        return mp4FileName.replace('.mp4','.ts');
-    },
-
-    createHierarchyPath: function(destPath, lastFileHash) {
-
-        let fileFullPath = destPath;
-        let hash = lastFileHash;
-
-        if(!preserveOriginalHLS) {
-            hash = new Date().getHours().toString();
-            fileFullPath = path.join(destPath, (hash) < 10 ? ("0" + hash) : hash);
-        }
-        let retVal = {fileFullPath, hash};
-        if (lastFileHash === hash)
-            return Q.resolve(retVal);
-
-        return qio.makeTree(fileFullPath)
-            .then(function() {
-                return retVal;
-            });
-    },
-
-    compressChunkName: preserveOriginalHLS ? function(tsChunkName){
-            return tsChunkName.replace('.ts','.mp4')
-        } : function(tsChunkName) {
-        var matched = tsChunktMatch.exec( tsChunkName );
-        if(matched){
-            return matched[1] + '-' + matched[2] + '.mp4';
-        }
-        return tsChunkName;
     }
 
-};
+    getRelativePathFromFull (fullPath) {
+        return fullPath.substr(this.getBasePathFromFull(fullPath).length);
+    }
+
+    getFlavorFullPath (entryId, flavorName) {
+        return path.join(config.get('rootFolderPath'), entryId, flavorName.toString());
+    }
+
+    getMasterManifestName() {
+        return 'playlist.json';
+    }
+
+    getMP4FileNamefromInfo(chunkPath){
+        return chunkPath.replace('.ts','.mp4');
+    }
+
+
+    getTSChunknameFromMP4FileName(mp4FileName){
+        return mp4FileName.replace('.mp4','.ts');
+    }
+
+    getEntryBasePath (entryId) {
+        return path.join(config.get('rootFolderPath'), entryId);
+    }
+
+
+}
+
+function createHierarchyPathHelper (fileFullPath, hash,lastFileHash) {
+    let retVal = {fileFullPath, hash};
+    if (lastFileHash === hash)
+        return Q.resolve(retVal);
+
+    return qio.makeTree(fileFullPath)
+        .then(function() {
+            return retVal;
+        });
+}
+
+if(!preserveOriginalHLS) {
+    class DefaultPersistenceFormat extends PersistenceFormatBase {
+        getBasePathFromFull(fullPath) {
+            // cut away both flavor and time components
+            let lastSepIdx = _.lastIndexOf(fullPath,path.sep) - 1;
+            return fullPath.substring(0,_.lastIndexOf(fullPath,path.sep,lastSepIdx)+1)
+        }
+
+        createHierarchyPath(destPath, lastFileHash) {
+
+            let hash = new Date().getHours().toString();
+            let fileFullPath = path.join(destPath, (hash) < 10 ? ("0" + hash) : hash);
+
+            return createHierarchyPathHelper(fileFullPath, hash, lastFileHash);
+        }
+
+        compressChunkName(tsChunkName) {
+            var matched = tsChunktMatch.exec(tsChunkName);
+            if (matched) {
+                return matched[1] + '-' + matched[2] + '.mp4';
+            }
+            return tsChunkName;
+        }
+
+    }
+    module.exports = new DefaultPersistenceFormat();
+} else {
+    class PreserveOriginalHLSFormat extends PersistenceFormatBase {
+        getBasePathFromFull(fullPath) {
+            return fullPath.substring(0,_.lastIndexOf(fullPath,path.sep)+1);
+        }
+
+        createHierarchyPath(destPath, lastFileHash) {
+            return createHierarchyPathHelper(destPath, lastFileHash, lastFileHash);
+        }
+
+        compressChunkName(tsChunkName) {
+            return super.getMP4FileNamefromInfo(tsChunkName);
+        }
+    }
+    module.exports = new PreserveOriginalHLSFormat();
+}
