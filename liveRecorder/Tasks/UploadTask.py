e56713e502e4d87a464633bc94677eb5dca65ce2
Ron Yadgar <ron5569@gmail.com>
2016-12-25 13:53:55 +0200

    add replacment status check for local mode also (#366)

--- liveRecorder/Tasks/UploadTask.py
+++ liveRecorder/Tasks/UploadTask.py
@@ -63,21 +63,25 @@ class UploadTask(TaskBase):
 
             if len(failed_jobs) == 0:
                 self.logger.info("successfully upload all chunks, call append recording")
-
-                #Check if need to call cancel_replace
-                recorded_obj = self.backend_client.get_recorded_entry(upload_session.partner_id, self.recorded_id)
-                if recorded_obj.replacementStatus.value != KalturaEntryReplacementStatus.NONE:
-                    self.logger.info("entry %s has replacementStatus %s, calling cancel_replace", self.recorded_id,
-                                    recorded_obj.replacementStatus)
-                    self.backend_client.cancel_replace(upload_session.partner_id, self.recorded_id)
+                self.check_replacment_status(upload_session.partner_id)
                 self.backend_client.set_recorded_content_remote(upload_session, str(float(self.duration)/1000))
                 os.rename(self.output_file_path, self.output_file_path + '.done')
             else:
                 raise Exception("Failed to upload file, "+str(len(failed_jobs))+" chunks from "+str(chunks_to_upload)+ " where failed:"
                                 + upload_session_json)
 
+    def check_replacment_status(self, partner_id):
+        self.logger.debug("About to check replacement status for [%s]", self.recorded_id)
+        recorded_obj = self.backend_client.get_recorded_entry(partner_id, self.recorded_id)
+        self.logger.debug("Got replacement Status: %s", recorded_obj.replacementStatus.value)
+        if recorded_obj.replacementStatus.value != KalturaEntryReplacementStatus.NONE:
+            self.logger.info("entry %s has replacementStatus %s, calling cancel_replace", self.recorded_id,
+                             recorded_obj.replacementStatus)
+            self.backend_client.cancel_replace(partner_id, self.recorded_id)
+
     def append_recording_handler(self):
         partner_id = self.backend_client.get_live_entry(self.entry_id).partnerId
+        self.check_replacment_status(partner_id)
         self.backend_client.set_recorded_content_local(partner_id, self.entry_id, self.output_file_path,
                                                        str(float(self.duration)/1000), self.recorded_id)
 
