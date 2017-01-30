5c687b53638754f70a5f7f3d44632667fbc13f48
Ron Yadgar <ron5569@gmail.com>
2017-01-30 17:10:26 +0200

    1.6 update daily reports (#390)
    * update
    
    * update
    
    * update

--- liveRecorder/DailyReport.py
+++ liveRecorder/DailyReport.py
@@ -20,18 +20,28 @@ def write_liveRecorede_stat(dataCenter, output_full_path, done_path, error_path,
     with open(output_full_path, 'a') as file_output:
         new_file_done, num_file_done = get_files(command + done_path, month, day)
         new_file_error, num_file_error = get_files(command + error_path, month, day)
-
         file_output.write("[" + dataCenter + "] Total files on done " + str(num_file_done) + "\n")
         file_output.write("[" + dataCenter + "] New files on done " + str(new_file_done) + "\n")
         file_output.write("[" + dataCenter + "] Total files on error " + str(num_file_error) + "\n")
         file_output.write("[" + dataCenter + "] New files on error " + str(new_file_error) + "\n")
 
 
-def get_logs(log_level, file_path):
+def get_live_controller_logs(log_level, file_path):
     command1 = ''.join(['zgrep ', '-a ', '-e  ', 'RecordingEntrySession ', '-e ', 'RecordingManager ', file_path])
     command2 = ''.join(['grep ', log_level])
     command = command1 + ' | ' + command2
-    print("About to run " +  command)
+    output = operate_command(command)
+    return output
+
+
+def get_live_recorder_logs(file_path):
+    command = ''.join(['zgrep ', '-a ', '-e  ', 'ERROR ', '-e ', 'WARN ', file_path])
+    output = operate_command(command)
+    return output
+
+
+def operate_command(command):
+    print("About to run " + command)
     output = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
     return output
 
@@ -39,8 +49,9 @@ def get_logs(log_level, file_path):
 def get_files(command, month, day):
     new_file = []
     lines_count=0
-    cmd = subprocess.Popen(command, shell=True,  stdout=subprocess.PIPE)
-    for line in cmd.stdout:
+    results = operate_command(command)
+    results = results.split('\n')
+    for line in results:
         lines_count = lines_count +1
         l = line.split( )
         if len(l) == 11 and l[5] == month and l[6] == str(day):
@@ -48,21 +59,31 @@ def get_files(command, month, day):
     return new_file, lines_count-1
 
 
-def scan_logs(log_level, output_full_path, now):
+def scan_logs_project(file_output, now, log_level, project):
+    month_new = "%02d" % (now.month,)
+    day_new = "%02d" % (now.day,)
+    src_dir = os.path.join('/web', 'logs', 'investigate', str(now.year), month_new, day_new, project)
+    for file_log in os.listdir(src_dir):
+        if file_log.endswith(".gz"):
+            file_path = os.path.join(src_dir, file_log)
+            try:
+                output = None
+                if project == 'liveController':
+                    output = get_live_controller_logs(log_level, file_path)
+                if project == 'live-recorder':
+                    output = get_live_recorder_logs(file_path)
+                if output is not None and len(output) != 0:
+                    file_output.write("\n \n \n \n Machine " + file_log + "\n")
+                    file_output.write(output)
+            except Exception as e:
+                print('Error on ' + file_log + e.message)
+
+
+def scan_logs(output_full_path, now):
     with open(output_full_path, 'a') as file_output:
-        monthNew =  "%02d" % (now.month,)
-        dayNew = "%02d" % (now.day,)
-        src_dir = os.path.join('/web', 'logs', 'investigate', str(now.year), monthNew, dayNew, 'liveController')
-        for file_log in os.listdir(src_dir):
-            if file_log.endswith(".gz"):
-                file_path = os.path.join(src_dir, file_log)
-                try:
-                    output = get_logs(log_level, file_path)
-                    if len(output) != 0:
-                        file_output.write("Machine " + file_log + "\n")
-                        file_output.write(output)
-                except Exception as e:
-                    print('Error on' + file_log)
+        scan_logs_project(file_output, now, "ERROR", 'liveController')
+        scan_logs_project(file_output, now, "None", 'live-recorder')
+        scan_logs_project(file_output, now, "WARN", 'liveController')
 
 
 def send_mail(send_from, send_to, subject, text, filePath=None, output_file=None, server="127.0.0.1"):
@@ -113,23 +134,22 @@ def get_report(mailAdress = None, relative_date = 1):
 
     write_liveRecorede_stat("pa", output_full_path, done_path, error_path, month, day)
     write_liveRecorede_stat("ny", output_full_path, done_path, error_path, month, day)
-    scan_logs('ERROR', output_full_path, now)
-    scan_logs('WARN', output_full_path, now)
+    scan_logs(output_full_path, now)
 
-    send_mail("pa-reportsk@jkaltura.com", mail_list, "DailyReport", "DailyReport", output_full_path, output_file)
+    send_mail("pa-reports@kaltura.com", mail_list, "DailyReport", "DailyReport", output_full_path, output_file)
     print("Send mail to " + str(mail_list) + " date:" + date)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser_argument_configure(parser)
     args = parser.parse_args()
-    if args.c is  None:
+    if args.c is None:
         relative_date = 1
         if args.date is not None:
-            relative_date = args.date
+            relative_date = int(args.date)
 
         get_report(mailAdress = args.mail, relative_date = relative_date)
 
     else:
-        for x in range(1, args.c):
+        for x in range(1, int(args.c)):
             get_report(mailAdress=args.mail, relative_date=x)
 
