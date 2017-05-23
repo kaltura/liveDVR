2cc5fb0627d4a3dca4908f18e63c62addd8acb30
Lilach Maliniak <lilach.maliniak@kaltura.com>
2017-05-23 10:11:28 +0300

    improve ts->mp4 convertor build script

--- liveRecorder/scripts/ts2mp4_build.sh
+++ liveRecorder/scripts/ts2mp4_build.sh
@@ -1,58 +1,65 @@
-# !/bin/bash
+#!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-    echo 'missing ffmpeg path parameter. example: ffmpeg_build ../../node_addons/FormatConverter/build/FFmpeg'
-    exit 1
+# default bash version in Darwin is currently (May 2017), 3.2.
+# the version doesn't support {$a,,} or {$a,} to change string to lower or upper case
+
+APP_NAME=liveRecorder
+PREPDIR="/opt/kaltura/builds/${APP_NAME}"
+BUILD_PATH=
+WEBAPPDIR=/web/kaltura/${APP_NAME}
+BUILD_FFMPEG_PATH=${WEBAPPDIR}/ffmpeg/ffmpeg-3.0
+ROOT_PATH=.
+TARGET=ts_to_mp4_convertor
+BIN_DIR=
+BUILD_FFMPEG_PATH=
+
+if [ -e $PREPDIR ]; then
+	ROOT_PATH==${PREPDIR}/${APP_VERSION}
+	CONVERTOR_DIR=${TARGET}
+	BUILD_PATH=${ROOT_PATH}/${$CONVERTOR_DIR}
+	BUILD_FFMPEG_PATH=${WEBAPPDIR}/ffmpeg/ffmpeg-3.0
+elif [ -e ../${TARGET} ]; then
+	CONVERTOR_DIR=../${TARGET}
+	BUILD_PATH=../${TARGET}
+	BUILD_FFMPEG_PATH=../../node_addons/FormatConverter/build/FFmpeg
 fi
 
-ffmpegLibsDir=$1
-echo ffmpeg lib dir: [$ffmpegLibsDir]
+if [ -e $CONVERTOR_DIR ]; then
+	echo "CONVERTOR_DIR=${CONVERTOR_DIR}, running make to create ${TARGET}"
+	echo uname=$(uname)
+	uname=$(uname)
+	OS=darwin
 
-currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo current dir: [$currentDir]
-
-ts2mp4ConverterDir="$currentDir/../../liveRecorder/ts_to_mp4_convertor"
-echo ts_to_mp4_convertor dir: [$ts2mp4ConverterDir]
-cd $ts2mp4ConverterDir
-
-[ -d "$ts2mp4ConverterDir/obj" ] || mkdir -p $ts2mp4ConverterDir/obj
-echo [$ts2mp4ConverterDir/obj]
-
-os_name=`uname`
-echo [$os_name]
-
-case  $os_name in
-"Darwin")
-    export OS="darwin"
-    ;;
-"Linux")
-	export OS="linux"
-   ;;
-esac
-
-echo os=[$OS]
-
-
-echo "********************"
-echo  Starting new build
-echo "********************"
-
-make FFMPEG_LIB_DIR=$ffmpegLibsDir
-
-echo "*******************"
-echo    Build Ended
-echo "*******************"
-
-
-[ -d "$currentDir/../bin/${OS}" ] || mkdir -p "$currentDir/../bin/${OS}"
-
-case  $os_name in
-"Darwin")
-    echo "Copying $ts2mp4ConverterDir/obj/ts_to_mp4_convertor to $currentDir/../bin/${OS}/"
-    cp $ts2mp4ConverterDir/obj/ts_to_mp4_convertor $currentDir/../bin/${OS}/
-    ;;
-"Linux")
-    echo "Copying $ts2mp4ConverterDir/obj/ts_to_mp4_convertor to $currentDir/../bin/${OS,,}/"
-    cp $ts2mp4ConverterDir/obj/ts_to_mp4_convertor $currentDir/../bin/${OS,,}/
-   ;;
-esac
+	pushd $BUILD_PATH
+		mkdir -p obj
+		[ -e  $BUILD_PATH/${TARGET} ] && rm -f ${TARGET}
+		[ -e  obj/${TARGET} ] && rm -f obj/${TRAGET}
+		if [ ${uname} != Darwin ]; then
+			OS=\${uname,,}
+			dpkg -l yasm || (echo "yasm is not installed, please install it" ; exit 1)
+		fi
+		BIN_DIR=${ROOT_PATH}/../bin/${OS}/
+		echo "OS=${OS}"
+		echo "BIN_DIR=${BIN_DIR}"
+		echo "make FFMPEG_LIB_DIR=${BUILD_FFMPEG_PATH}"
+		make FFMPEG_LIB_DIR=${BUILD_FFMPEG_PATH}
+		if [ -s  ${BUILD_PATH}/obj/${TARGET} ] ; then
+			echo "**************************************************************************************"
+			echo "${TARGET} was built successfully, copying to bin folder"
+			echo "**************************************************************************************"
+			mkdir -p ${ROOT_PATH}/bin/${OS}
+			echo "cp ${BUILD_PATH}/obj/${TARGET} ${BIN_DIR}"
+			cp ${BUILD_PATH}/obj/${TARGET} ${BIN_DIR}
+		else
+			echo "**************************************************************************************"
+			echo "Something is wrong, failed to build ts_to_mp4_convertor!!!, please check build results"
+			echo "**************************************************************************************"
+			exit 1
+		fi
+	popd
+else
+	echo "**************************************************************************************"
+	echo "${ROOT_DIR}/${$CONVERTOR_DIR} folder is missing, can't build ${TARGET}"
+	echo "**************************************************************************************"
+	exit 1
+fi
