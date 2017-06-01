8c1b3f5f287ffd7021a9b86cd6fca542114b438f
Lilach Maliniak <lilach.maliniak@kaltura.com>
2017-06-01 10:50:41 +0300

    minor update of log message

--- liveRecorder/Tasks/ConcatinationTask.py
+++ liveRecorder/Tasks/ConcatinationTask.py
@@ -172,7 +172,7 @@ class ConcatenationTask(TaskBase):
                 self.logger.info('Successfully finished TS -> MP4 conversion')
             else:
                 status = 'failed'
-                error = 'Failed to convert TS -> MP4. Convertor process exit code {}, {}'.format(process.returncode), outerr
+                error = 'Failed to convert TS -> MP4. Convertor process exit code {}, {}'.format(exitcode), outerr
                 self.logger.error(error)
 
                 raise subprocess.CalledProcessError(exitcode, command)
