0cecf9c14b3853ca8c046bacb7ff862b8e3fa621
gadyaari <gad.yaari@kaltura.com>
2017-06-04 13:40:43 +0300

    PLAT-7476: Create a script adding new server-node to database and enabling it.

--- serviceWrappers/linux/addServerNode.js
+++ serviceWrappers/linux/addServerNode.js
@@ -1,18 +1,21 @@
 /**
  * Created by user on 01/06/2017.
  */
-const BackendClient = require('../../lib/BackendClient.js');
+const BackendClient = require('../../lib/BackendClient');
 const ErrorUtils = require('../../lib/utils/error-utils');
+const Hostname = require('../../common/utils/hostname');
 
-console.log("Adding server node [" + process.argv[2] + "]");
-return BackendClient.addServerNode(process.argv[2])
+// If hostname is transferred as a parameter take its value, otherwise get hostname of local machine.
+let hostname = (process.argv[2]) ? process.argv[2] : Hostname.getLocalMachineFullHostname();
+console.log("Adding server node [" + hostname + "]");
+return BackendClient.addServerNode(hostname)
     .then((apiResult) => {
         console.log("API call headers: " + apiResult.headers);
         console.log("API call result: " + apiResult.result);
         if (apiResult.err || apiResult.result.objectType === 'KalturaAPIException') {
             throw new Error("Failed to add serverNode. " + ErrorUtils.error2string(apiResult.err));
         }
-        console.log("ServerNode [" + process.argv[2] + "] was successfully added to server");
+        console.log("ServerNode [" + hostname + "] was successfully added to server");
         return (apiResult.result);
     })
     .then((result) => {
