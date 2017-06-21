94fa715e440364af531d0dc31ee4aae3266325f4
Lilach Maliniak <lilach.maliniak@kaltura.com>
2017-06-21 09:44:40 +0300

    updates based on comments from code review
    main changes:
    name refactor: ts2mp4_build.sh -> build_ts2mp4_convertor.sh
    updated the above script to run with 2 command line parameters as
    follow: build_ts2mp4_convertor <ffmpeg build path> <product path>

--- liveRecorder/scripts/build_ts2mp4_convertor.sh
+++ liveRecorder/scripts/build_ts2mp4_convertor.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
 
-APP_NAME=liveRecorder
-PREPDIR="/opt/kaltura/builds/${APP_NAME}"
-BUILD_PATH=
-WEBAPPDIR=/web/kaltura/${APP_NAME}
-BUILD_FFMPEG_PATH=${WEBAPPDIR}/ffmpeg/ffmpeg-3.0
-ROOT_PATH=.
-TARGET=ts_to_mp4_convertor
-BIN_DIR=${ROOT_PATH}/../bin/
-BUILD_FFMPEG_PATH=
-BUILD_PATH=${CONVERTOR_DIR}/obj
-
-if [ -e $PREPDIR ]; then
-	ROOT_PATH==${PREPDIR}/${APP_VERSION}
-	CONVERTOR_DIR=${ROOT_PATH}/${TARGET}
-	BUILD_FFMPEG_PATH=${WEBAPPDIR}/ffmpeg/ffmpeg-3.0
-else
-	SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
-	CONVERTOR_DIR=${SCRIPT_DIR}/../${TARGET}
-	BUILD_FFMPEG_PATH="../../node_addons/FormatConverter/build/FFmpeg"
+if [ "$#" -lt 2 ]; then
+	echo "usage build_ts2mp4_convertor <ffmpeg build path> <product path>"
+	exit
 fi
 
-TARGET_FULL_PATH=${BUILD_PATH}/${TARGET}
-export BUILD_FFMPEG_PATH
-echo "FFMPEG_SOURCE_DIR=${FFMPEG_SOURCE_DIR}"
-echo "TARGET_FULL_PATH=${TARGET_FULL_PATH}"
-BASH_VERSION="${BASH_VERSINFO[0]}.${BASH_VERSINFO[1]}"
-echo BASH_VERSION=${BASH_VERSION}
-echo "BIN_DIR=${BIN_DIR}"
-echo "make FFMPEG_SOURCE_DIR=${BUILD_FFMPEG_PATH}"
-echo uname=$(uname)
+FFMPEG_BUILD_PATH=$1
+PRODUCT_ROOT_PATH=$2
+TARGET=ts_to_mp4_convertor
+CONVERTOR_DIR=${PRODUCT_ROOT_PATH}/${TARGET}
+
+export FFMPEG_BUILD_PATH
+
+echo "OS=$(uname)"
 
 
 if [ -e $CONVERTOR_DIR ]; then
@@ -38,7 +21,7 @@ if [ -e $CONVERTOR_DIR ]; then
 		[ -e  ${TARGET} ] && rm -f ${TARGET}
 		[ -e  obj/${TARGET} ] && rm -f obj/${TARGET}
 
-		echo "starting to build ${TARGET} from ${PWD}"
+		echo "starting to build ${TARGET}"
 
 		make
 
--- scripts/build_ffmpeg.sh
+++ scripts/build_ffmpeg.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 if [ "$#" -lt 2 ]; then
-	echo "usage build_ffmpeg <product path> <ffmpeg build path> <release/debug - optional>"
+	echo "usage build_ffmpeg <ffmpeg build path> <product path> <release/debug - optional>"
 	exit
 fi
 
@@ -15,59 +15,53 @@ FFMPEG_SYMLINK=$PRODUCT_ROOT_PATH/node_addons/FormatConverter/build/FFmpeg
 [ ! -e $FFMPEG_BUILD_PATH ] || mkdir -p "$FFMPEG_BUILD_PATH"
 [ -z "$BUILD_CONF" ] && BUILD_CONF=Release
 
+echo "current path `pwd`"
+echo "FFMPEG_BUILD_PATH=$FFMPEG_BUILD_PATH"
+echo "FFMPEG_SYMLINK=$FFMPEG_SYMLINK"
 
-function makeFFmpeg()
-{
-	echo "current path `pwd`"
-	echo "FFMPEG_BUILD_PATH=$FFMPEG_BUILD_PATH"
-	echo "FFMPEG_SYMLINK=$FFMPEG_SYMLINK"
+if [ -d $FFMPEG_BUILD_PATH ]; then
 
-    if [ -d $FFMPEG_BUILD_PATH ]; then
-
-        if [ -L "$FFMPEG_SYMLINK" ]; then
-            rm $FFMPEG_SYMLINK
-        fi
-
-        curl -OL https://github.com/FFmpeg/FFmpeg/releases/download/n3.0/ffmpeg-3.0.tar.gz
-        mv ./ffmpeg-3.0.tar.gz /var/tmp/ffmpeg-3.0.tar.gz
-
-        tar -xzvf /var/tmp/ffmpeg-3.0.tar.gz -C $FFMPEG_BUILD_PATH
-        ln -s $FFMPEG_BUILD_PATH/ffmpeg-3.0 $FFMPEG_SYMLINK
+    if [ -L "$FFMPEG_SYMLINK" ]; then
+        rm $FFMPEG_SYMLINK
     fi
 
-    pushd $FFMPEG_SYMLINK
+    curl -OL https://github.com/FFmpeg/FFmpeg/releases/download/n3.0/ffmpeg-3.0.tar.gz
+    mv ./ffmpeg-3.0.tar.gz /var/tmp/ffmpeg-3.0.tar.gz
 
-        echo "current path `pwd`"
+    tar -xzvf /var/tmp/ffmpeg-3.0.tar.gz -C $FFMPEG_BUILD_PATH
+    ln -s $FFMPEG_BUILD_PATH/ffmpeg-3.0 $FFMPEG_SYMLINK
+fi
 
-        debug_specifics=
-        [ "$BUILD_CONF" = "Debug" ] &&  debug_specifics='--enable-debug --disable-optimizations'
+pushd $FFMPEG_SYMLINK
 
-        configFileName=./lastConfigure
+    echo "current path `pwd`"
 
-        confCmd="./configure --disable-everything --disable-doc --enable-protocol=file --enable-demuxer=mpegts --enable-muxer=rtp_mpegts --enable-parser=h264 --enable-parser=aac --enable-muxer=mp4 --enable-zlib --enable-bsf=aac_adtstoasc --enable-decoder=aac --enable-decoder=h264 --enable-muxer=flv --enable-protocol=rtmp --enable-encoder=libmp3lame $debug_specifics"
+    debug_specifics=
+    [ "$BUILD_CONF" = "Debug" ] &&  debug_specifics='--enable-debug --disable-optimizations'
 
-        [ "$os_name" == "Linux" ] && confCmd="$confCmd --enable-pic"
+    configFileName=./lastConfigure
 
-        actualCmd=""
+    confCmd="./configure --disable-everything --disable-doc --enable-protocol=file --enable-demuxer=mpegts --enable-muxer=rtp_mpegts --enable-parser=h264 --enable-parser=aac --enable-muxer=mp4 --enable-zlib --enable-bsf=aac_adtstoasc --enable-decoder=aac --enable-decoder=h264 --enable-muxer=flv --enable-protocol=rtmp --enable-encoder=libmp3lame $debug_specifics"
 
-       [ -f "$configFileName" ] && actualCmd=`cat $configFileName`
+    [ "$os_name" == "Linux" ] && confCmd="$confCmd --enable-pic"
 
-       echo -e "actualCmd=\n<$actualCmd>"
-       echo -e "confCmd=\n<$confCmd>"
-       if [ "$actualCmd" != "$confCmd" ]
-       then
-          echo "configuring ffmpeg..."
-          eval "$confCmd"
-       else
-          echo "no need to run configure"
-       fi
+    actualCmd=""
 
-       echo "$confCmd" > $configFileName
+   [ -f "$configFileName" ] && actualCmd=`cat $configFileName`
 
-       make &> /dev/null
-    popd
+   echo -e "actualCmd=\n<$actualCmd>"
+   echo -e "confCmd=\n<$confCmd>"
+   if [ "$actualCmd" != "$confCmd" ]
+   then
+      echo "configuring ffmpeg..."
+      eval "$confCmd"
+   else
+      echo "no need to run configure"
+   fi
 
-}
+   echo "$confCmd" > $configFileName
 
-makeFFmpeg
+   make &> /dev/null
+popd
+
 
