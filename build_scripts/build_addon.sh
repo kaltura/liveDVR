69bf87b0af198392d928c9b272480c2491bf5d11
Lilach Maliniak <lilach.maliniak@IL-LILACHM-BE-1889.local>
2017-06-25 17:29:02 +0300

    updates based on review with Guy E.

--- build_scripts/build_addon.sh
+++ build_scripts/build_addon.sh
@@ -1,25 +1,41 @@
-# !/bin/bash
+#!/bin/bash
+
+#===============================================================================
+#          FILE: build_addon.sh
+#         USAGE: ./deploy_liveRecorder.sh
+#   DESCRIPTION:
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR:  (),
+#  ORGANIZATION: Kaltura, inc.
+#       CREATED:
+#      REVISION:  ---
+#===============================================================================
 
 if [ "$#" -lt 1 ]; then
 	echo "usage build_addon <product path> [Release/Debug]"
-	exit
+	exit 1
 fi
 
 PRODUCT_ROOT_PATH=$1
 BUILD_CONF=Release
-ADDON_PATH=$PRODUCT_ROOT_PATH/node_addons/FormatConverter
+ADDON_PATH=${PRODUCT_ROOT_PATH}/node_addons/FormatConverter
 FORMAT_CONVERTER_BIN=FormatConverter.so
 OS=`uname`
+RES=0
+
 echo "OS=$OS"
 
-[ "$#" -eq 2 ] && [ "$2" = "Debug" ] && BUILD_CONF=$2
+[ "$2" = "Debug" ] && BUILD_CONF=Debug
 
-echo "BUILD_CONF=$BUILD_CONF"
-echo "ADDON_PATH=$ADDON_PATH"
+echo "BUILD_CONF=${BUILD_CONF}"
+echo "ADDON_PATH=${ADDON_PATH}"
 
 mkdir -p ${PRODUCT_ROOT_PATH}/bin
 
-pushd $ADDON_PATH
+pushd ${ADDON_PATH}
 
 	`which node-gyp` || npm install node-gyp -g
 
@@ -28,12 +44,12 @@ pushd $ADDON_PATH
 
 	gyp_args=''
 
-	case $OS in
+	case ${OS} in
 	'Darwin')
 	    echo "Mac OS"
 	    gyp_args='-- -f xcode'
-	    echo "$gyp_args"
-	    node-gyp configure $gyp_args
+	    echo "${gyp_args}"
+	    node-gyp configure ${gyp_args}
 	    FORMAT_CONVERTER_BIN=FormatConverter.dylib
 	    ;;
 	*) ;;
@@ -44,14 +60,22 @@ pushd $ADDON_PATH
 
 	debugExt=''
 
-	if [ "$BUILD_CONF" = "Debug" ]; then
+	if [ "${BUILD_CONF}" = "Debug" ]; then
 	    gyp_debug="--debug"
 	    debugExt=".debug"
 	fi
-	echo "Start node-gyp build. $gyp_debug"
-	node-gyp build $gyp_debug -v
+	echo "Start node-gyp build. ${gyp_debug}"
+	node-gyp build ${gyp_debug} -v
 
-	echo "cp build/${BUILD_CONF}/${FORMAT_CONVERTER_BIN} ${PRODUCT_ROOT_PATH}/bin/FormatConverter.node$debugExt"
-	cp "build/${BUILD_CONF}/${FORMAT_CONVERTER_BIN}" "${PRODUCT_ROOT_PATH}/bin/FormatConverter.node$debugExt"
+    if [ -r "build/${BUILD_CONF}/${FORMAT_CONVERTER_BIN}"  ]; then
+		echo "cp build/${BUILD_CONF}/${FORMAT_CONVERTER_BIN} ${PRODUCT_ROOT_PATH}/bin/FormatConverter.node${debugExt}"
+		cp "build/${BUILD_CONF}/${FORMAT_CONVERTER_BIN}" "${PRODUCT_ROOT_PATH}/bin/FormatConverter.node${debugExt}"
+		echo "### build finished successfully"
+	else
+		echo "### build failed, could not find build/${BUILD_CONF}/${FORMAT_CONVERTER_BIN}, check for errors"
+		RES=1
+	fi
 
 popd
+
+exit ${RES}
--- build_scripts/build_ts2mp4_convertor.sh
+++ build_scripts/build_ts2mp4_convertor.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+#===============================================================================
+#          FILE: build_ts2mp4_convertor.sh
+#         USAGE: ./deploy_liveRecorder.sh
+#   DESCRIPTION:
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR:  (),
+#  ORGANIZATION: Kaltura, inc.
+#       CREATED:
+#      REVISION:  ---
+#===============================================================================
+
 if [ "$#" -lt 2 ]; then
 	echo "usage build_ts2mp4_convertor <ffmpeg build path> <product path>"
-	exit
+	exit 1
 fi
 
 FFMPEG_BUILD_PATH=$1
 PRODUCT_ROOT_PATH=$2
 TARGET=ts_to_mp4_convertor
 CONVERTOR_DIR=${PRODUCT_ROOT_PATH}/${TARGET}
+RES=0
 
 export FFMPEG_BUILD_PATH
 
-echo `uname`
 
-
-if [ -w $CONVERTOR_DIR ]; then
-	pushd $CONVERTOR_DIR
+if [ -w ${CONVERTOR_DIR} ]; then
+	pushd ${CONVERTOR_DIR}
 		mkdir -p obj
-		mkdir -p ../bin
 
 		echo "starting to build ${TARGET}"
 
@@ -32,12 +44,14 @@ if [ -w $CONVERTOR_DIR ]; then
 			echo "**************************************************************************************"
 			echo "Something went wrong, failed to build ts_to_mp4_convertor!!!, please check build results"
 			echo "**************************************************************************************"
-			exit 1
+			RES=1
 		fi
 	popd
 else
 	echo "**************************************************************************************"
 	echo "${ROOT_DIR}/${CONVERTOR_DIR} folder is missing, can't build ${TARGET}"
 	echo "**************************************************************************************"
-	exit 1
+	RES=1
 fi
+
+exit ${RES}
