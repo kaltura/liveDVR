20f890a490ab5825a1278ad3465fba881d451d43
Lilach Maliniak <lilach.maliniak@IL-LILACHM-BE-1889.local>
2017-06-26 17:55:46 +0300

    updated get-bins.sh to use loop

--- deployment/get_bins.sh
+++ deployment/get_bins.sh
@@ -14,43 +14,39 @@
 #===============================================================================
 
 if [ "$#" -lt 1 ]; then
-	echo "usage get_bins <version> [live root path]"
+	echo "usage get_bins <VERSION> [live root path]"
 	exit 1
 fi
 
-live_root_path=
+LIVE_ROOT_PATH=
 
-[ "$#" -eq 2 ] && live_root_path=$2
+[ "$#" -eq 2 ] && LIVE_ROOT_PATH=$2
 
-version=$1
+VERSION=$1
 
-echo "starting to download FormatConverter.node"
-wget --header="accept-encoding: gzip" http://lna-ci-slave2.kaltura.com/bin/$version/linux/release/FormatConverter.node -O FormatConverter.node.gz
-echo "extracting FormatConverter.node.gz"
-gunzip FormatConverter.node.gz
-if [ ! -w $live_root_path ]; then
-	mv FormatConverter.node $version/bin
-else
-   mv FormatConverter.node $live_root_path/bin
-fi
-echo "successfully downloaded FormatConverter.node"
-echo "starting to download nginx"
-wget --header="accept-encoding: gzip" http://lna-ci-slave2.kaltura.com/bin/$version/linux/release/nginx -O nginx.gz
-echo "extracting nginx.gz"
-gunzip nginx.gz
-if [ ! -w $live_root_path ]; then
-	mv nginx $version/bin
-else
-	mv nginx $live_root_path/bin
-fi
-echo "successfully downloaded nginx"
-echo "starting to download ts_to_mp4_convertor"
-wget --header="accept-encoding: gzip" http://lna-ci-slave2.kaltura.com/bin/$version/linux/release/ts_to_mp4_convertor -O ts_to_mp4_convertor.gz
-echo "extracting ts_to_mp4_convertor.gz"
-gunzip ts_to_mp4_convertor.gz
-if [ ! -w $live_root_path ]; then
-	mv ts_to_mp4_convertor $version/liveRecorder/bin
-else
-    mv ts_to_mp4_convertor $live_root_path/liveRecorder/bin
-fi
-echo "successfully downloaded ts_to_mp4_convertor"
+FILES_TO_DOWNLOAD=(FormatConverter.node nginx ts_to_mp4_convertor)
+DEST_PATH=(bin bin liveRecorder/bin)
+BASE_URL=http://lna-ci-slave2.kaltura.com/bin/$VERSION/linux/release/
+
+function download_files()
+{
+	for ((i=0;i<${#FILES_TO_DOWNLOAD[@]};++i))
+	do
+	    local filename="${FILES_TO_DOWNLOAD[$i]}"
+	    local dest="${DEST_PATH[$i]}"
+		echo "starting to download ${filename}"
+		wget --header="accept-encoding: gzip" ${BASE_URL}${filename} -O ${filename}.gz
+		echo "extracting ${filename}.gz"
+		gunzip ${filename}.gz
+		if [ ! -w ${LIVE_ROOT_PATH} ]; then
+			echo "moving ${filename} to $VERSION/${dest}"
+			mv -f ${filename} $VERSION/${dest}
+		else
+			echo "moving ${filename} to ${LIVE_ROOT_PATH}/${dest}"
+		    mv -f ${filename} ${LIVE_ROOT_PATH}/${dest}
+		fi
+		echo "successfully downloaded ${filename}"
+	done
+}
+
+download_files
