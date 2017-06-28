4216e5f34681260ff7438b370407737c60413b34
Lilach Maliniak <lilach.maliniak@IL-LILACHM-BE-1889.local>
2017-06-28 15:35:06 +0300

    adding lot's of comments and fixing some issues

--- deployment/get_bins.sh
+++ deployment/get_bins.sh
@@ -27,32 +27,40 @@ OS=linux
 
 VERSION=$1
 
+# array of binaries to download from jenkins
 FILES_TO_DOWNLOAD=(FormatConverter.node nginx ts_to_mp4_convertor)
+# array with relative destination paths for the downloaded binaries
 DEST_PATH=(bin bin liveRecorder/bin)
-BASE_URL=http://lna-ci-slave2.kaltura.com/bin/$VERSION/${OS}/release
+# Jenkins contains latest built binaries in latest dir and each built binary is also copied to latest dir
+# in case bin was not built in version the latest will be downloaded
+VERSION_URL=http://lna-ci-slave2.kaltura.com/bin/$VERSION/${OS}/release
 LATEST_URL=http://lna-ci-slave2.kaltura.com/bin/latest/${OS}/release
 
 function download_files()
 {
+	# loop to download the binaries
 	for ((i=0;i<${#FILES_TO_DOWNLOAD[@]};++i))
 	do
 	    local filename="${FILES_TO_DOWNLOAD[$i]}"
 	    local dest="${DEST_PATH[$i]}"
 		echo "starting to download ${filename}"
-		echo "wget --header="accept-encoding: gzip" ${BASE_URL}/${filename} -O ${filename}.gz"
-		wget --header="accept-encoding: gzip" ${BASE_URL}/${filename} -O ${filename}.gz
+		echo "wget --header="accept-encoding: gzip" ${VERSION_URL}/${filename} -O ${filename}.gz"
+		wget --header="accept-encoding: gzip" ${VERSION_URL}/${filename} -O ${filename}.gz
+		# if failed to download binary from version url try to downlaod from latest url
 		if [ $? -ne 0 ]; then
-		    echo "wget ${BASE_URL}/${filename} returned $?"
+		    echo "wget ${VERSION_URL}/${filename} returned $?"
 			wget --header="accept-encoding: gzip" ${LATEST_URL}/${filename} -O ${filename}.gz
+			if [ $? -ne 0 ]; then
+                echo "error $?, returned from wget --header="accept-encoding: gzip" ${LATEST_URL}/${filename} -O ${filename}.gz"
+                exit 1;
+            else
+                echo "successfully downloaded ${LATEST_URL}/${filename}"
+            fi
 		else
-			echo "successfully downloaded ${BASE_URL}/${filename}"
+			echo "successfully downloaded ${VERSION_URL}/${filename}"
 		fi
-		if [ $? -ne 0 ]; then
-			echo "error $?, returned from wget --header="accept-encoding: gzip" ${LATEST_URL}/${filename} -O ${filename}.gz"
-			exit 1;
-		else
-			echo "successfully downloaded ${LATEST_URL}/${filename}"
-		fi
+
+		# After downloading file, move it to its destination dir
 		echo "extracting ${filename}.gz"
 		gunzip ${filename}.gz
 		if [ ! -w ${LIVE_ROOT_PATH} ]; then
