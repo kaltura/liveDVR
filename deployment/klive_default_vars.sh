8895790c49e63934c2e94d6ea4ec6204a457ee74
Lilach Maliniak <lilach.maliniak@IL-LILACHM-BE-1889.local>
2017-07-25 09:29:29 +0300

    updated scripts to support default vars to set tokens in services wrapper scripts
    pre testing

---
+++ deployment/klive_default_vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# sets default variables as environment vars, for usage by liveRecorder and liveController service wrapper scripts
+export KLIVE_LOG_DIR=/var/log
+export KLIVE_PID_DIR=/var/run
+export KLIVE_CONTROLLER_USE_NVM="1"
+export KLIVE_CONTROLLER_PREFIX=/opt/kaltura/liveController/latest
+
+
