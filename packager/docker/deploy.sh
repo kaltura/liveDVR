62db86a1f0c7e7cda67f4a9900ebdfb4981d84d5
kalturaguy <guy.jacubovski@kaltura.com>
2018-01-18 09:20:33 +0200

    packager works!

--- packager/docker/deploy.sh
+++ packager/docker/deploy.sh
@@ -7,17 +7,20 @@ tar -zxvf nginx-${nginxVersion}.tar.gz
 rm nginx-${nginxVersion}.tar.gz -f
 mv nginx-${nginxVersion} nginx
 
-mkdir /opt/nginx
 echo "pulling nginx-secure-token-module"
-
 git clone https://github.com/kaltura/nginx-secure-token-module
+
 echo "pulling nginx-vod-module"
 git clone https://github.com/kaltura/nginx-vod-module
 
 echo "pulling nginx_requestid"
 git clone https://github.com/kaltura/nginx_requestid
 
-echo "pulling nginx_requestid"
+echo "pulling headers-more-nginx-module"
+git clone https://github.com/openresty/headers-more-nginx-module
+
+echo "pulling headers-more-nginx-module"
+git clone https://github.com/openresty/headers-more-nginx-module
+
+echo "pulling nginx_mod_akamai_g2o"
 git clone https://github.com/kaltura/nginx_mod_akamai_g2o
-
-
