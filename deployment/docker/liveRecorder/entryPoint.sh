32c63456c7dc7d6fb3f00b35cd7946bfd64f5338
kalturaguy <guy.jacubovski@kaltura.com>
2018-02-01 18:16:17 +0200

    add liverecorder (not working yet)

---
+++ deployment/docker/liveRecorder/entryPoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+LOGDIR=/var/log/liveRecorder
+HOSTNAME=`hostname`
+RECORDING_FOLDER=/web/content/kLive/liveRecorder
+
+
+sed -e "s#@HOSTNAME@#.*#g" \
+    -e "s#@RECORDING_FOLDER@#$RECORDING_FOLDER#g" \
+    -e "s#@KALTURA_PARTNER_ADMIN_SECRET@#$PARTNER_ADMIN_SECRET#g" \
+    -e "s#@KALTURA_PARTNER_ID@#$PARTNER_ID#g" \
+    -e "s#@VOD_UPLOAD_MODE@#remote#g" \
+    -e "s#@LIVE_PACKAGER_TOKEN@#$PACKAGER_SECURE_TOKEN#g" \
+    -e "s#@LOGS_BASE_PATH@#$LOGDIR#g" \
+    -e "s#@KALTURA_SERVICE_URL@#$SERVICE_URL#g" \
+    ./Config/configMapping.ini.template > ./Config/configMapping.ini
+
+pwd
+cat  ./Config/configMapping.ini
+
+createFolders() {
+	[ -d ${LOGDIR} ] || mkdir -p ${LOGDIR}
+	[ -d ${RECORDING_FOLDER} ] || mkdir -p ${RECORDING_FOLDER}
+
+	SHARED_APP_DIR=$RECORDING_FOLDER
+	echo "Creating folders in ${SHARED_APP_DIR} for ${HOSTNAME}"
+	if [ -d  ${SHARED_APP_DIR} ] ; then
+		mkdir -p ${SHARED_APP_DIR}/${HOSTNAME}/{UploadTask/{incoming,failed,processing},ConcatenationTask/{failed,processing}}
+		[ -L ${SHARED_APP_DIR}/${HOSTNAME}/ConcatenationTask/incoming ] || ln -s ${SHARED_APP_DIR}/incoming ${SHARED_APP_DIR}/${HOSTNAME}/ConcatenationTask/incoming
+	else
+		echo "can't find ${SHARED_APP_DIR}, cannot start, check mounts"
+		exit 2
+	fi
+}
+
+echo "starting liveRecorder..."
+createFolders || exit 4
+python main.py
+echo "ok!"
+
+
