f9f2fc5d448fe3dba64cfb48cda0f7d1c101e2c6
kalturaguy <guy.jacubovski@kaltura.com>
2018-03-15 11:55:33 +0200

    add test server for debugging

--- liveRecorder/ts_to_mp4_convertor/test.py
+++ liveRecorder/ts_to_mp4_convertor/test.py
@@ -1,15 +1,48 @@
 from optparse import OptionParser
 import glob,subprocess
-
+import json
+import re
+import urllib
 from datetime import datetime
 
 def log_subprocess_output(process):
     while True:
         nextline = process.stdout.readline()
+        nextlineerr = process.stderr.readline()
         if nextline == '' and process.poll() is not None:
             break
         print nextline
+        print nextlineerr
 
+
+lang_map = {
+    111: ("swe","Swedish"),
+    110: ("fin","Finnish")
+}
+def probe_file(filename):
+    match= re.search(r"_f(\d+)_out", filename)
+    lang=None
+    if match:
+        a=match.groups()
+        flavorId = int(a[0])
+        lang= lang_map.get(flavorId,None)
+    args = ['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams','-i', filename]
+    p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result,err = p.communicate()
+    j=json.loads(result)
+
+    main_stream =j['streams'][0]
+    if main_stream['codec_type']=='video':
+        resolution = str(main_stream['width'])+'x'+str(main_stream['height'])
+    else:
+        resolution = None
+
+    return {
+        'duration': j['format']['duration'],
+        'bitrate':  (8*float(j['format']['size'])) / float(j['format']['duration']),
+        'resolution': resolution,
+        'lang': lang
+    }
 
 def convert_ts_to_mp4( command):
 
@@ -20,9 +53,9 @@ def convert_ts_to_mp4( command):
     print 'About to run TS -> MP4 conversion. Command: %s' % command
 
     try:
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        process = subprocess.Popen(command, shell=True)
 
-        log_subprocess_output(process)
+        #log_subprocess_output(process)
 
         output, outerr = process.communicate()
         exitcode = process.returncode
@@ -64,8 +97,30 @@ files = glob.glob(options.directory+"/*.ts")
 
 command = options.executable
 
+manifest=""
+
 for file in files:
-    command += " " +file + " "+file.replace('.ts', '.mp4')+ " und "
+    mp4 = file.replace('.ts', '.mp4')
+    command += " " +file + " "+mp4+ " und "
+    res=probe_file(file)
+
+    if res.get('lang'):
+        manifest='#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"audio\",LANGUAGE=\"{0}\",NAME=\"{1}\",AUTOSELECT=YES,DEFAULT=YES,URI=\"{2}\"\n'.format(res['lang'][0],res['lang'][1],mp4)\
+                  +manifest
+    else:
+        if res.get('resolution'):
+            manifest+="#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH={0:.0f},RESOLUTION={1},AUDIO=\"audio\"\n".format(res['bitrate'],res['resolution'])
+        else:
+            manifest+="#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH={0:.0f},AUDIO=\"audio\"\n".format(res['bitrate'])
+
+        manifest+="http://localhost:8081/serve{0}/hls/index.m3u8\n".format(mp4)
 
 
+manifest = "#EXTM3U\n" + manifest
+
+master_manifest = options.directory+"/master.m3u8";
+with open(master_manifest, "w") as text_file:
+    text_file.write(manifest)
+
+print "https://video-dev.github.io/hls.js/demo/?src={0}".format(urllib.quote_plus("http://localhost:8081/manifest/"+master_manifest))
 convert_ts_to_mp4(command)
