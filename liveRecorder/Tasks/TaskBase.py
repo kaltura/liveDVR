64e3f8d0bde03f89bf76d2ff1d68fd0f52ffb34e
David Winder <david.winder@kaltura.com>
2018-06-06 11:59:32 +0300

    Add key fame accuracy in nginx

--- liveRecorder/Tasks/TaskBase.py
+++ liveRecorder/Tasks/TaskBase.py
@@ -39,9 +39,8 @@ class TaskBase(object):
         return None
 
     def update_status(self, new_status):
-        data = self.get_data()
-        if data and data.get('taskId',None):
-            id = data.get('taskId',None)
+        if self.data and self.data.get('taskId',None):
+            id = self.data.get('taskId',None)
             self.logger.debug("Updating taskId: [{}] with new status: [{}]".format(id, new_status))
             self.backend_client.update_task_entryServerNode_status(id, new_status)
 
@@ -59,6 +58,7 @@ class TaskBase(object):
                                            self.entry_directory)
         self.stamp_full_path = os.path.join(self.recording_path, 'stamp')
         self.data_full_path = os.path.join(self.recording_path, 'data.json')
+        self.data = self.get_data()
         self.backend_client = BackendClient(self.entry_id + '-' + self.recorded_id)
 
 
