14bdd7842f52c7fbf4be9160cc9da51c63c5c4df
kalturaguy <guy.jacubovski@kaltura.com>
2019-01-27 19:45:05 +0200

    initial commit

--- deployment/docker/liveController/entryPoint.sh
+++ deployment/docker/liveController/entryPoint.sh
@@ -3,28 +3,43 @@
 source initScript.sh
 
 echo applying new configMapping
-
 LOG_FILE_NAME=/var/log/liveController/kLiveController.log
-CONTENT_DIR=/web/content/kLive
-LIVE_CONTENT_PATH=${CONTENT_DIR}/live
-LIVE_ARCHIVE_CONTENT_PATH=${CONTENT_DIR}/archive
-WOWZA_ADMIN_USER=wowza
-WOWZA_ADMIN_PASSWORD=wowza
-RECORDING_FOLDER=${CONTENT_DIR}/liveRecorder
-WOWZA_HOSTNAME=media-server
+LIVE_CONTENT_PATH=/web/content/kLive/live
+LIVE_ARCHIVE_CONTENT_PATH=$/web/content/kLive/archive
+RECORDING_FOLDER=/web/content/kLive/liveRecorder
 
 
-sed -e "s#@LIVE_CONTENT_PATH@#$LIVE_CONTENT_PATH#g" \
-    -e "s#@LIVE_ARCHIVE_CONTENT_PATH@#$LIVE_ARCHIVE_CONTENT_PATH#g" \
-    -e "s#@LOG_FILE_NAME@#$LOG_FILE_NAME#g" \
-    -e "s#@KALTURA_SERVICE_URL@#$SERVICE_URL#g" \
-    -e "s#@KALTURA_PARTNER_ADMIN_SECRET@#$PARTNER_ADMIN_SECRET#g" \
-    -e "s#@KALTURA_PARTNER_ID@#$PARTNER_ID#g" \
-    -e "s#@HOSTNAME@#$SERVER_NODE_HOST_NAME#g" \
-    -e "s#@WOWZA_ADMIN_USER@#$WOWZA_ADMIN_USER#g" \
-    -e "s#@WOWZA_ADMIN_PASSWORD@#$WOWZA_ADMIN_PASSWORD#g" \
-    -e "s#@RECORDING_FOLDER@#$RECORDING_FOLDER#g" \
-    ./common/config/configMapping.json.template > ./common/config/configMapping.json
+jsonFile=$1;
+
+node > ./common/config/configMapping.json << EOF
+const fs=require("fs");
+//Read data
+let data = JSON.parse(fs.readFileSync('./common/config/configMapping.json.template', 'utf8'));
+let config=data[".*"];
+config.rootFolderPath="$LIVE_CONTENT_PATH";
+config.oldContentFolderPath="$LIVE_ARCHIVE_CONTENT_PATH";
+config.logFileName="$LOG_FILE_NAME";
+config.logToConsole = true;
+
+config.backendClient.serviceUrl="$SERVICE_URL";
+config.backendClient.adminSecret="$PARTNER_ADMIN_SECRET";
+config.backendClient.partnerId=$PARTNER_ID;
+
+config.mediaServer.hostname="$SERVER_NODE_HOST_NAME";
+config.mediaServer.user="$WOWZA_ADMIN_USER";
+config.mediaServer.password="$WOWZA_ADMIN_PASSWORD";
+config.mediaServer.wowzaHost="$WOWZA_HOSTNAME";
+config.mediaServer.wowzaMetadataHost="$WOWZA_METADATA_HOST";
+config.mediaServer.port=80;
+
+config.recording.recordingFolderPath= "$RECORDING_FOLDER/recordings";
+config.recording.completedRecordingFolderPath= "$RECORDING_FOLDER/incoming";
+
+//Output data
+console.log(JSON.stringify(data,null,2));
+
+EOF
+
 
 cat ./common/config/configMapping.json
 
